58df1f19365101b91cd09beee554d93a2966d828
Ali Paikan <ali.paikan@gmail.com>
2014-05-28 22:26:54 +0200

    more tests

--- tests/binding_c_complex/src/main.adb
+++ tests/binding_c_complex/src/main.adb
@@ -1,15 +1,18 @@
 
-with Ada.Text_IO;       use Ada.Text_IO;
-with random_num_h;      use random_num_h;
-with Interfaces.C;      use Interfaces.C;
+with Ada.Text_IO;           use Ada.Text_IO;
+with Interfaces.C;          use Interfaces.C;
+with Interfaces.C.Strings;  use Interfaces.C.Strings;
+with sqlite3_h;             use sqlite3_h;
+with System;
 
 procedure Main is
+    db : System.Address;
+    ret : Integer;
+--    db_name : Strings.chars_ptr := New_String("test.db");
 begin
     Put_Line("This is an example of using C funtion in Ada!");
+    ret := Integer(sqlite3_open(New_String("test.db"), db));
 
-    for i in 1 .. 10 loop
-        Put_Line(Integer'Image( Integer(GetRandom(int(1),int(10))) ));
-    end loop;
 end Main;
 
 
--- tests/binding_c_complex/src_c/random_num.h
+++
@@ -1,7 +0,0 @@
-#ifndef GUARD_random_number_h
-#define GUARD_random_number_h
- 
-int getRandom(int a, int b);
-
-#endif
-
